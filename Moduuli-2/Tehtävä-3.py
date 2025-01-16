d66a4f28544fcf8bca0aa99dd0e74a6b70731a65
Jazuu236 <jasper.t.jarvinen@gmail.com>
2025-01-16 12:45:25 +0200

    Moduuli 2

--- Moduuli-2/Tehtävä-3.py
+++ Moduuli-2/Tehtävä-3.py
@@ -2,6 +2,6 @@ print("Syötä suorakulmion kanta ja korkeus saadaksesi sen pinta-alan ja piirin
 kanta = float(input("Kanta:"))
 korkeus = float(input("Korkeus:"))
 pinta_ala = kanta*korkeus
-piiri = 2*kanta+2*korkeus
+piiri = 2*(kanta+korkeus)
 print(f"{"Pinta-ala"}: {pinta_ala:10.2f}")
 print(f"{"Piiri"}:     {piiri:10.2f}")
